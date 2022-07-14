110fca38334fb52cca2e33d111fdea3ac91df51b
Cloudbop <boppincolin@gmail.com>
2022-07-14 13:22:36 +0100

    +cnt

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,9 +1,10 @@
+// can be included in 'test' command script in package.json
 import { expect, it } from 'vitest'
+//
 import { add } from './basic-testing-frontend/src/math'
 it('should summarise numbers values of array together', () => {
 
   const r = add([1, 2, 3])
-
   expect(r).toEqual(6)
 
 })
