994bfa001791bb36fc27ff68c1f4dcd112d52661
Cloudbop <boppincolin@gmail.com>
2022-07-14 13:21:46 +0100

    first test

---
+++ basic-testing-frontend/src/math.test.js
@@ -0,0 +1,11 @@
+import { expect, it } from 'vitest'
+import { add } from './basic-testing-frontend/src/math'
+it('should summarise numbers values of array together', () => {
+
+  const r = add([1, 2, 3])
+
+  expect(r).toEqual(6)
+
+})
+
+
