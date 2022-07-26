b2cdc09380585572533b2cd3de9232a83e9ab5c1
Cloudbop <boppincolin@gmail.com>
2022-07-26 17:47:45 +0100

    refactored app to enable testing handler callback

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,9 +1,25 @@
+import { cleanNumbers } from './util/numbers.js';
+
 export function add(numbers) {
   let sum = 0;
 
+  // throw new Error('Something went wrong');
+
   for (const number of numbers) {
-    // will NaN if not coercable to number
-    sum += Number(number);
+    sum += +number;
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  let result = '';
+  try {
+    const numbers = cleanNumbers(numberValues);
+    console.log('result', numbers)
+
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+  return result;
 }
