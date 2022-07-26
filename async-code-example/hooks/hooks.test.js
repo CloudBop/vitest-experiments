696b5c85b0192a33de7475503ddb8a0c49bab5ed
Cloudbop <boppincolin@gmail.com>
2022-07-26 20:58:21 +0100

    +concurrent

--- async-code-example/hooks/hooks.test.js
+++ async-code-example/hooks/hooks.test.js
@@ -23,7 +23,7 @@ afterEach(() => {
 afterAll(() => {
 })
 
-it('should update the email', () => {
+it.concurrent('should update the email', () => {
 
   const newTestEmail = 'test2@test.com';
 
@@ -31,26 +31,32 @@ it('should update the email', () => {
   expect(user.email).toBe(newTestEmail);
 });
 
-it('should have an email property', () => {
+it.concurrent('should have an email property', () => {
 
   expect(user).toHaveProperty('email');
 });
 
-it('should store the provided email value', () => {
+it.concurrent('should store the provided email value', () => {
 
   expect(user.email).toBe(testEmail);
 });
 
-it('should clear the email', () => {
+it.concurrent('should clear the email', () => {
 
   user.clearEmail();
 
   expect(user.email).toBe('');
 });
 
-it('should still have an email property after clearing the email', () => {
+it.concurrent('should still have an email property after clearing the email', () => {
 
   user.clearEmail();
 
   expect(user).toHaveProperty('email');
 });
+
+/**
+ * Even when not adding the .concurrent property / annotation, tests that are stored in different files are executed concurrently (i.e., in parallel). This is done by both Vitest and Jest - ensuring that your tests run in a short amount of time.
+With .concurrent you can enforce this behavior also inside the individual files (i.e., tests that live in one and the same file are executed concurrently).
+Concurrent execution can reduce the amount of time your tests need to execute. A downside of concurrent execution is, that tests that perform clashing (global) state manipulations may interfere with each other.
+ */
