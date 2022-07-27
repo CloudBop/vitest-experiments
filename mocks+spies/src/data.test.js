bc9e3d28b576460d61782708e152222939ab00be
Cloudbop <boppincolin@gmail.com>
2022-07-27 16:52:07 +0100

    +spy instead of invoking console.log

---
+++ mocks+spies/src/data.test.js
@@ -0,0 +1,18 @@
+import {
+  describe, it, expect,
+  // similar in jest
+  vi
+} from "vitest";
+import { generateReportData } from "./data";
+
+
+describe("generateReportData()", () => {
+  it("should execute logFn if provided", () => {
+    // create spy -> don't need to invoke/test the actual console.log()->[sideeffect]
+    const logger = vi.fn();
+    //
+    generateReportData(logger)
+    // has our spy been called.
+    expect(logger).toBeCalled();
+  })
+})
