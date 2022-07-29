6dc5db2fdab24630b321267ba0d4553fae0f4edf
Cloudbop <boppincolin@gmail.com>
2022-07-29 21:15:30 +0100

    +correctly implement mock

--- mocks+spies/src/util/io.test.js
+++ mocks+spies/src/util/io.test.js
@@ -1,5 +1,6 @@
 import { it, expect, vi } from "vitest";
 import writeData from './io';
+import { promises as fs } from 'fs';
 //
 // - overrule fs implementation, this will make current test fail as the fs (invoked in io.js) wont return a promise!
 vi.mock('fs');
@@ -9,8 +10,10 @@ it("should execute the writeFile method [sideffect]", () => {
   const testData = 'test';
   const testFileName = 'test.txt';
 
-  //we wouldn't want to actually test the implementation, in this case nodejs filesystem.
-  // as this could mean mutating raw data on the system
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  // invoke to assert that the mock was called.
+  writeData(testData, testFileName);
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
   // if filepath ("../../data/ ") doesn't exist it will fail
+  //we wouldn't want to actually test the implementation, in this case nodejs filesystem. as this could mean mutating raw data on the system
 })
