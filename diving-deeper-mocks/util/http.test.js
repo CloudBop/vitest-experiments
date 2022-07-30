405216c2caa85325cb8583dc1585379c84c272a8
Cloudbop <boppincolin@gmail.com>
2022-07-30 23:10:25 +0100

    +overrule default fetch() +test

--- diving-deeper-mocks/util/http.test.js
+++ diving-deeper-mocks/util/http.test.js
@@ -2,12 +2,27 @@ import { describe, it, expect, vi } from 'vitest';
 import { sendDataRequest } from './http';
 // import { HttpError, ValidationError } from "./errors"
 
-
+const testResponseData = { testKey: "testData" }
+// spy/track 
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    const testResponse = {
+      ok: true,
+      json() {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData)
+        })
+      }
+    }
+    resolve(testResponse)
+  })
+});
 // 
-// overrule vi blobal 'fetch()'
-//
-vi.stubGlobal('fetch', () => { });
+// overrule vitest global 'fetch()'
+vi.stubGlobal('fetch', testFetch);
 
 it('should return any available response data', () => {
-  sendDataRequest();
+  const testData = { key: 'test' }
+
+  return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
 })
